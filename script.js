c0cb30968da0dcf9ae87ef24e998da0825bf8785
Tropic groupe <84191908+tropic-groupe@users.noreply.github.com>
2022-01-05 17:46:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const sr = ScrollReveal();
+
+sr.reveal ('.title', {
+    distance: '100px',
+    duration: 1000,
+    reset: true,
+})
+
+sr.reveal ('.box', {
+    distance: '100px',
+    duration: 1000,
+    reset: true, 
+})
+sr.reveal ('#id2', {
+    distance: '25px',
+    duration: 2500,
+    reset: true,
+})
+sr.reveal ('#id3', {
+    distance: '50px',
+    duration: 3000,
+    reset: true,
+})
